26a2c07f8b83a44bbd50b3f055c74ab2fe6b82f9
Juanshot <garcia.solutions@gmail.com>
2016-01-06 03:08:17 -0430

    timeline

--- js/controllers/partidos.js
+++ js/controllers/partidos.js
@@ -1,14 +1,26 @@
 app
-   .controller('PartidosCtrl', function($scope,ComentarioResource) {
+   .controller('PartidosCtrl', function($scope,PartidosResource) {
     $scope.title = "Crear Comentario";
     $scope.button = "Guardar";
     $scope.Comentario = {};
     $scope.saveComentario = function(){
        // console.log($scope.Comentario);
-        ComentarioResource.save($scope.Comentario);
+        PartidosResource.save($scope.Comentario);
     }
 })
-.controller('GetPartidosCtrl', function($scope,ComentarioResource) {
-   $scope.Comentarios = ComentarioResource.query();
+.controller('GetPartidosCtrl', function($scope,PartidosResource) {
+   $scope.partidos= PartidosResource.query();
+   console.log($scope.partidos);
+   
+   $scope.formVisibility = false;
+   $scope.show_timeline = function(id){
+       if($scope.formVisibility == true){
+           $scope.formVisibility = false
+       }else{
+           $scope.formVisibility = true 
+       }
+       
+       
+   }
 })
 ;
--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ angular.module('app')
 
       // config
       $scope.app = {
-        name: 'Futboleros',
+        name: 'La Hinchada',
         version: '1.3.3',
         // for chart colors
         color: {
