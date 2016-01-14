1664d7670e361d5f7a220c076f59a03cb563e2f2
Juanshot <garcia.solutions@gmail.com>
2016-01-14 01:08:13 -0430

    l

--- js/controllers/partidos.js
+++ js/controllers/partidos.js
@@ -19,10 +19,252 @@ app
            $scope.formVisibility = true 
        }
            $scope.comentarios = comentarios;
-             console.log(comentarios);  
+             console.log(comentarios);
+             
          $scope.trustAsHtml = function(value) {
             return $sce.trustAsHtml(value);
         };
+        $scope.vistaComentario= function(comentario){
+            var sentido = '';
+            var sentido2 = '';
+              switch (comentario.tipo_comentario_id){
+                  
+               case 0 :
+                 if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-falta'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                case 1 :
+                    if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-fuera'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                     case 2 :
+                      if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-tiro'>\n\
+                              <span class='tl-date'>"+comentario.minuto+".Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    case 3 :
+                    if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-gol'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" :Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                 <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    case 4 :
+                     if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-gol'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" :Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    case 5 :
+                    if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-medio'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" :Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                 <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    case 6 :
+                     if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-extra'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" :Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                 <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    case 7 :
+                     if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-final'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" :Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                 <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    case 8 :
+                     if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-amarilla'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" :Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                 <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    case 9 :
+                     if(comentario.equipo_calidad == 1){
+                       sentido = 'tl-item';
+                       sentido2 = 'arrow left pull-up';
+                   }else if (comentario.equipo_calidad == 2){
+                       sentido = 'tl-item tl-left';
+                       sentido2 = 'arrow right pull-up visible-left';
+                   }
+                     return "<li class='"+sentido+"'>\n\
+                             <div class='tl-wrap b-roja'>\n\
+                              <span class='tl-date'>"+comentario.minuto+" :Min.</span>\n\
+                              <div class='tl-content panel padder b-a w-md w-auto-xs'>\n\
+                                <span class='"+sentido2+"'></span>\n\
+                                <div class='text-lt m-b-sm'>"+comentario.titulo+"</div>\n\
+                                <div class='panel-body pull-in b-t b-light'>\n\
+                                <a href='' class='thumb pull-right m-l m-t-xs avatar'>\n\
+                                        <img src='img/a4.jpg' alt='...'>\n\
+                                            <i class='on md b-white bottom'></i> <a>\n\
+                                                <div class='clear'>\n\
+                                                   <a href='' class='text-primary block m-b-xs'>"+comentario.contenido+" <i class='icon-twitter'></i></a>\n\
+                                                            </div>  </div></div></div> </li>"            
+                                         
+                    break;
+                    
+                    
+                    
+                    
+        }
+        }
        
        
    }
